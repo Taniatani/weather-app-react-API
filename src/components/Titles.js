0482ac749b8f45b8439d613071a3d6e2700a7941
Tania Akter <taniatani51@gmail.com>
2018-05-10 15:43:47 +0600

    Adding components

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component{
+	render() {
+		return (
+			<div>
+				<h2>Know your outside world</h2>
+			</div>
+		);
+	}
+}
+
+
+export default Titles;
